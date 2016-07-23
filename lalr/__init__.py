9fbc50f0257e53a8a877363e4420e58f111de362
Ben Mather <bwhmather@bwhmather.com>
2016-07-23 12:48:41 +0100

    Partial attempt at combining construction of follow sets and transitions

--- lalr/__init__.py
+++ lalr/__init__.py
@@ -1,4 +1,5 @@
-_START_SYMBOL = '_S' # object()
+_START_SYMBOL = '_S'  # TODO
+_EOF = '_$'  # TODO
 
 
 class Production(object):
@@ -28,6 +29,9 @@ class Production(object):
             name=self.name,
             symbols=' '.join(str(symbol) for symbol in self.symbols),
         )
+
+    def __repr__(self):
+        return "[" + str(self) + "]"
 
     def __eq__(self, other):
         return (
@@ -60,6 +64,12 @@ class Grammar(object):
     def symbols(self):
         return set.union(self.terminals, self.nonterminals)
 
+    def is_terminal(self, symbol):
+        return symbol in self.terminals
+
+    def is_nonterminal(self, symbol):
+        return symbol in self.nonterminals
+
     def productions(self, name=None):
         if name is None:
             return set(self._productions)
@@ -71,7 +81,7 @@ class Grammar(object):
 
 
 class Queue(object):
-    def __init__(self, items):
+    def __init__(self, items=[]):
         self._queued = list(items)
         self._all = set(items)
 
@@ -97,7 +107,10 @@ class Queue(object):
         return self
 
     def __next__(self):
-        return self.pop()
+        try:
+            return self.pop()
+        except IndexError:
+            raise StopIteration()
 
 
 def build_first_sets(grammar):
@@ -153,25 +166,26 @@ def build_first_sets(grammar):
 
 class Item(object):
 
-    __slots__ = ('_production', '_cursor')
+    __slots__ = ('_production', '_cursor', '_follow_set')
 
-    def __init__(self, production, cursor=0):
+    def __init__(self, production, cursor, follow_set):
         assert cursor <= len(production)
 
         # TODO should this be an identifier instead of a concrete object
         self._production = production
         self._cursor = cursor
+        self._follow_set = frozenset(follow_set)
 
     def __hash__(self):
         return (
             hash(self.production) ^
-            hash(self.cursor)
+            hash(self._cursor)
         )
 
     def __eq__(self, other):
         return (
             self.production == other.production and
-            self.cursor == other.cursor
+            self._cursor == other._cursor
         )
 
     @property
@@ -192,44 +206,54 @@ class Item(object):
 
     @property
     def matched(self):
-        return tuple(self.production[:self.cursor])
+        return tuple(self.production[:self._cursor])
 
     @property
     def expected(self):
-        return tuple(self.production[self.cursor:])
+        return tuple(self.production[self._cursor:])
+
+    @property
+    def follow_set(self):
+        return set(self._follow_set)
 
     def __str__(self):
-        return "{name} → {matched} • {expected}".format(
-            name = self.name,
+        return "{name} → {matched} • {expected}, {follow_set}".format(
+            name=self.name,
             matched=' '.join(str(symbol) for symbol in self.matched),
             expected=' '.join(str(symbol) for symbol in self.expected),
+            follow_set='/'.join(str(symbol) for symbol in self.follow_set),
         )
 
     def __repr__(self):
         return '[' + str(self) + ']'
 
     def shift(self):
-        if self.cursor >= len(self.production):
+        if self._cursor >= len(self.production):
             raise Exception("out of bounds")
 
         return Item(self.production, cursor=self.cursor)
 
 
 class ItemSet(object):
-
     def __init__(self, kernel, derived):
-        self._kernel = kernel
-        self._derived = derived
+        self._kernel = frozenset(kernel)
+        self._derived = frozenset(derived)
 
     @property
     def kernel(self):
-        raise NotImplementedError()
+        return set(self._kernel)
+
+    @property
+    def derived(self):
+        if self._derived is None:
+            self._derived = _build_derived_items(self.grammar, self.kernel)
+        return set(self._derived)
 
     @property
     def items(self):
         return set.union(
-            self._kernel,
-            self._derived,
+            self.kernel,
+            self.derived,
         )
 
     def terminals(self):
@@ -260,27 +284,139 @@ class ItemSet(object):
 
 
 def _build_derived_items(grammar, kernel):
-    symbols = set()
-    queue = set()
-    derived = set()
+    """Given a core set of items and a grammar, recursively expand
+    non-terminals into new items until there are no non-terminals left.
 
-    def _queue_symbols_from_item(item):
-        symbols.add(item.name)
-        if item.expected:
-            symbol = item.expected[0]
-            if symbol not in symbols:
-                queue.add(symbol)
+    :param grammar:
+        A grammar, obviously.
+
+    :param kernel:
+        A :class:`set` of :class:`Item`s that make up the kernel of an item set
+    """
+    # We assume that (with the exception of the starting symbol which we can't
+    # reach from any other rule), the cursor will never appear at the beginning
+    # of an item in a kernel.  This means that items in the kernel will not
+    # also be in the derived set.
+
+    # A map from symbols to sets of terminals that can follow them.  This is
+    # used at the end when transforming productions to items
+    follow_sets = {}
+
+    # Queue of symbols that should be processed into items
+    symbol_queue = Queue()
+
+    grammar.first_sets = build_first_sets(grammar)  # TODO TODO TODO
 
     for item in kernel:
-        _queue_symbols_from_item(item)
-        
-    while queue:
-        for production in grammar.productions(queue.pop()):
-            item = Item(production, 0)
-            _queue_symbols_from_item(item)
-            derived.add(item)
+        if not item.expected:
+            continue
 
-    return derived
+        symbol, *rest = item.expected
+
+        if grammar.is_terminal(symbol):
+            continue
+
+        # Add new non-terminals to the follow set
+        follow_set = follow_sets.setdefault(symbol, set())
+
+        if rest:
+            follow_set.update(grammar.first_sets[rest[0]])
+        else:
+            follow_set.update(item.follow_set)
+
+        # Queue for processing
+        symbol_queue.add(symbol)
+
+    productions = set()
+
+    immediate_dependants = {}
+
+    # Expand set of derived productions
+    for symbol in symbol_queue:
+        for production in grammar.productions(symbol):
+            productions.add(production)
+
+            # Find the next symbol and the tuple of symbols following it.
+            # As the grammar should have been converted to epsilon free form,
+            # and the cursor will always be at the beginning, we can be sure
+            # that there will be at least one item and the check can be skipped
+            first, *rest = production.symbols
+
+            if grammar.is_terminal(first):
+                continue
+
+            symbol_queue.add(first)
+
+            # First we update the follow sets for each symbol.
+            # As cycles are possible we need to make sure that we update the
+            # follow sets of non-terminals that we have already expanded
+            if rest:
+                new_items = grammar.first_sets[rest[0]]
+            else:
+                new_items = follow_sets.get(production.name, set())
+
+                # Make sure that the first symbol gets updated when new items
+                # are added to the follow set of the current symbol
+                immediate_dependants.setdefault(
+                    production.name, set()
+                ).update(first)
+
+            follow_sets.setdefault(first, set()).update(new_items)
+
+            # The set of non-terminals that can be reduced to the current
+            # symbol, either directly or via some number of intermediate steps
+            dependants = set()
+
+            # We build up the set of dependants by iterating over the symbols
+            # that are a dependant of the first symbol, and recursively over
+            # their dependants, processing each symbol only once.
+            # The dependants queue keeps track of what has been done, and what
+            # needs to be done.
+            dependants_queue = Queue({first})
+
+            # Build the set
+            for dependant in dependants_queue:
+                if dependant not in immediate_dependants:
+                    continue
+
+                dependants_queue.update(immediate_dependants[dependant])
+                dependants.update(immediate_dependants[dependant])
+
+            # Update the follow sets of all dependant symbols
+            for dependant in dependants:
+                follow_sets[dependant].update(new_items)
+
+    # Turn productions and follow sets into items
+    return frozenset(
+        Item(production, cursor=0, follow_set=follow_sets[production.name])
+        for production in productions
+    )
+
+
+
+def merge_items(a, b):
+    if a.production != b.production or a.cursor != b.cursor:
+        raise Exception('Item cores do not match')
+
+    return Item(a.production, a.cursor, set.union(a.follow_set, b.follow_set))
+
+
+def merge_item_sets(sa, sb):
+    assert sa.grammar == sb.grammar
+
+    a_items_by_core = {
+        (item.matched, item.expected): item for item in sa.kernel
+    }
+    b_items_by_core = {
+        (item.matched, item.expected): item for item in sa.kernel
+    }
+
+    assert set(a_items_by_core) == set(b_items_by_core)
+
+    return ItemSet(grammar, {
+        merge_items(a_items_by_core[core], b_items_by_core[core])
+        for core in a_items_by_core
+    })
 
 
 def build_item_set(grammar, kernel):
@@ -291,32 +427,44 @@ def item_set_transitions(grammar, item_set):
     kernels = {}
 
     for item in item_set.items:
-        if item.expected:
-            symbol = item.expected[0]
-            kernel = kernels.setdefault(symbol, set())
-            kernel.add(Item(item.production, item.cursor + 1))
+        if not item.expected:
+            continue
 
-    # TODO maybe it would make sense to return only the kernel as we have to
-    # deduplicate the generated item sets.  Generating each set more than once
-    # takes more work, and deduplicating item sets is more expensive than
-    # kernels as well
-    return {
-        symbol: build_item_set(grammar, kernel)
-        for symbol, kernel in kernels.items()
-    }
+        symbol, *rest = item.expected
 
+        kernel = kernels.setdefault(symbol, set())
+        kernel.add(Item(item.production, item.cursor + 1, item.follow_set))
+
+    return kernels
+
+def _kernel_core(kernel):
+    return frozenset((item.matched, item.expected) for item in kernel)
 
 def build_transition_table(grammar, target):
     '''Build the item sets, and map out the corresponding transitions for a
     grammar that accepts the given target.
     '''
-    starting_item = Item(Production(_START_SYMBOL, {target,}))
+    starting_item = Item(
+        Production(_START_SYMBOL, {target, }), cursor=0, follow_set={_EOF},
+    )
 
     # A list of item sets.  Item sets are identified by index.  We initialise
     # it with a new item set with the start symbol as its kernel
-    item_sets = [
-        build_item_set(grammar, {starting_item})
+    item_sets = []
+ 
+    # A list of item set kernels.  This is gradually processed into the list
+    # of item sets
+    kernels = [
+        {starting_item},
     ]
+
+    # A map from item set cores, frozen sets of tuples of visited and expected
+    # strings without a follow set, to item set indexes into the item set
+    # array.  This is only used internally.
+    # TODO does the starting item need to be put in this map?
+    kernels_by_core = {
+        _kernel_core(kernels[0]): 0,
+    }
 
     # A list, with items corresponding to the sets `item_sets`  of
     # dictionaries mapping from symbols to item set indexes
@@ -326,11 +474,25 @@ def build_transition_table(grammar, target):
     # through adding the corresponding transition dictionaries.  If we get to
     # processing the last item set in the list and no more item sets need to be
     # added, then we are done.
-    while len(transitions) < len(item_sets):
-        target_item_set = item_sets[len(transitions)]
+    while len(item_sets) < len(kernels):
+        target_item_set = build_item_setitem_sets[len(transitions)]
         target_transitions = {}
 
-        for symbol, item_set in item_set_transitions(grammar, target_item_set).items():
+        for symbol, item_set in item_set_transitions(
+            grammar, target_item_set
+        ).items():
+            # This merge step is what makes this an LALR parser generator.
+            # In an LR parser generator, we would include the follow sets in
+            # the comparison and dispense with the merging.
+            item_set_core = _item_set_core(item_set)
+            if item_set_core not in item_sets_by_core:
+                item_sets.append(item_set)
+                item_sets_by_core[item_set_core] = len(item_sets)
+            else:
+                item_sets[item_sets_by_core[item_sets]] = merge_item_sets(
+                    item_sets[item_sets_by_core[item_set_core]], item_set,
+                )
+
             # TODO complexity here could be much better
             if item_set not in item_sets:
                 item_sets.append(item_set)
@@ -342,78 +504,62 @@ def build_transition_table(grammar, target):
     return item_sets, transitions
 
 
-def build_first_sets(grammar):
-    """Returns a map from symbols to sets of terminals that can appear as the
-    first terminal in that symbol.  Terminal symbols obviously just map to a
-    set containing only themselves
-
-    This can be created without needing to look at item sets.
-    This requires that the grammar rules are epsilon free.
-    """
-    # A map from symbols to sets of symbols for which there exist productions
-    # where the first symbol is the first element
-    has_first_symbol = {}
-
-    for rule in rules:
-        has_first_symbol.setdefault(rule.name, set()).add(rule.symbols[0])
-
-    # A map from symbols to the set of terminals which can appear as the first
-    # terminal in a string that the symbol matches
-    first_sets = {}
-
-    for terminal in grammar.terminals:
-        # TODO if this is needed it should be folded into the inner loop.
-        # kept separate for now so that it's very clear what is happening
-        first_sets[terminal] = {terminal}
-
-        queue = list(has_first_symbol[terminal])
-
-        # The set of non-terminals to which the terminal has been added.  This
-        # is checked before adding more non-terminals to the queue so we do not
-        # need to check if a terminal has already been added to the first set
-        # when pulling an item off it.
-        done = set()
-
-        while queue:
-            non_terminal = queue.pop()
-            first_sets.setdefault(non_terminal, set()).add(terminal)
-            # It is important to add the non-terminal to done before updating
-            # the queue to avoid infinite loops on self-recursive symbols
-            done.add(non_terminal)
-            queue.add(set.difference(has_symbol_as_first[non_terminal], done))
-
-    assert (
-        set(first_sets) == set.union(grammar.terminals, grammar.non_terminals)
-    )
-
-    # item sets should contain only terminals
-    assert not any(
-        set.difference(item_set, grammar.terminals)
-        for item_set in item_sets.values()
-    )
-
-    return first_sets
-
-
-def build_follow_sets(grammar, *args):
-    """Build a map from non-terminals to sets of terminals that can come
+def build_follow_sets(grammar, first_sets, item_sets, transitions):
+    """Build a map from item sets to sets of terminals that can come
     immediately after them.
+
+    The map is represented as an array with elements corresponding to the
+    elements in `item_sets`.
     """
-    for non_terminal in grammar.non_terminals:
+
+    for nonterminal in grammar.nonterminals:
         pass
 
     for production in grammar.productions:
         for symbol in production.symbols:
             pass
-            
 
-def build_action_table(grammar, *args):
+
+def build_shift_table(grammar, *args):
+    '''Returns a list of maps from terminal symbols to shift actions.
+
+    A shift action is simply an index into the item_set array.
+    '''
+    pass
+
+
+def build_goto_table(grammar, *args):
+    '''Returns a list of dictionaries mapping from non-terminal symbols to the
+    state that should follow.
+
+    The items in the list correspond to items in the list of item sets.
+    '''
+    pass
+
+
+def build_reduction_table(grammar, *args):
+    '''Returns a list of dictionaries mapping from terminal symbols to reduce
+    actions.
+
+    Reduce actions are represented simply by a reference to a rule.
+    The items in the list of reduction dictionaries correspond to items in the
+    list of item sets.
+    '''
+    pass
+
+
+def check_shift_reduce_conflicts():
+    '''Check for conflicts between a shift table and a reduce table
+    '''
+    pass
+
+
+def build_parse_table():
     '''
     :returns:
         A list of dictionaries mapping from terminals to parser actions.  The
         items in the list correspond to the grammar parse states.
     '''
-
     def _make_shift():
         def _shift(parser):
             parser.stack.push(state)
@@ -426,102 +572,3 @@ def build_action_table(grammar, *args):
             parser.stack.push(gotos[rule.name])
 
         return _reduce
-
-
-def build_goto_table(grammar, *args):
-    '''
-    :returns:
-        A dictionary mapping from non-terminal identifiers to state identifiers
-    '''
-    pass
-
-
-class ParserState(object):
-    def __init__(self, parser, tokens):
-        self._tokens = iter(tokens)
-        self.advance()
-
-    def peek(self):
-        return self._current
-
-    def advance(self):
-        self._current = next(self._input)
-
-    def shift(self):
-        raise NotImplementedError()
-
-    def reduce(self):
-        raise NotImplementedError()
-
-
-class Parser(object):
-    def __init__(self, item_sets, actions, gotos):
-        """
-        :param item_sets:
-            A list of :class:`ItemSet` instances.
-        :param actions:
-            A list of maps from terminal symbols to actions.  The items in the
-            list correspond to states in the item set list.  An action is TODO
-        :param gotos:
-            A list of map from non-terminal symbols to state indices.  The
-            items in the list correspond to states in the item set list, as do
-            the indices in the maps.
-        """
-        # TODO figure out if it would make more sense to build these in the
-        # constructor from the grammar
-        self._item_sets = item_sets
-        self._actions = actions
-        self._gotos = gotos
-
-    def parse(self, tokens):
-        state = ParserState(self, token)
-
-        for token in tokens:
-            action = self.action(token)
-            action(state)
-
-
-def parse(string, item_sets, actions, gotos):
-    '''
-    '''
-    state_stack = [0]
-    result_stack = []
-
-    def _peek():
-        return string[0]
-
-    def _advance():
-        string = string[1:]
-
-    def _push(state):
-        stack.append(state)
-
-    def _pop(count):
-        stack = stack[:-count]
-
-    def _shift(state):
-        result_stack.push(_peek())
-        _advance()
-        _push(state)
-
-    def _reduce(rule):
-        _pop(len(rule))
-        _push(gotos[rule.name])
-
-    while True:
-        pass
-        #symbol = _peek()
-        #action = actions[_state()][symbol]
-        #case action of:
-        #    Shift state -> _shift(state)
-        #    Reduce rule -> _reduce(rule)
-        #    Accept -> return
-        #    Nothing -> raise Exception()
-
-
-
-
-
-
-
-
