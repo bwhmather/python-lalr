dd8f5753a790f3c8970481538b3d57bab4f4dc4b
Ben Mather <bwhmather@bwhmather.com>
2016-08-02 22:05:18 +0100

    Make first_sets a proper attribute of Grammar

--- lalr/__init__.py
+++ lalr/__init__.py
@@ -74,11 +74,65 @@ class Production(object):
         )
 
 
+def build_first_sets(grammar):
+    """Returns a map from symbols to sets of terminals that can appear as the
+    first terminal in that symbol.  Terminal symbols obviously just map to a
+    set containing only themselves
+
+    This can be created without needing to look at item sets.
+    This requires that the grammar rules are epsilon free.
+    """
+    # A map from symbols to sets of symbols for which there exist productions
+    # where the first symbol is the first element
+    has_first_symbol = {}
+
+    for production in grammar.productions():
+        has_first_symbol.setdefault(
+            production.symbols[0], set()
+        ).add(production.name)
+
+    # A map from symbols to the set of terminals which can appear as the first
+    # terminal in a string that the symbol matches
+    first_sets = {}
+
+    for terminal in grammar.terminals:
+        # TODO if this is needed it should be folded into the inner loop.
+        # kept separate for now so that it's very clear what is happening
+        first_sets[terminal] = {terminal}
+
+        if terminal not in has_first_symbol:
+            continue
+
+        queue = Queue(has_first_symbol[terminal])
+
+        while queue:
+            nonterminal = queue.pop()
+            first_sets.setdefault(nonterminal, set()).add(terminal)
+
+            queue.update(has_first_symbol.get(nonterminal, set()))
+
+    assert (
+        set(first_sets) == set.union(grammar.terminals, grammar.nonterminals)
+    )
+
+    # First sets should contain only terminals
+    assert not any(
+        set.difference(first_set, grammar.terminals)
+        for first_set in first_sets.values()
+    )
+
+    # There should be a first set for every terminal and non-terminal
+    assert grammar.symbols == set(first_sets)
+
+    return first_sets
+
+
 class Grammar(object):
 
     def __init__(self, productions, terminals):
         self._productions = frozenset(productions)
         self._terminals = frozenset(terminals)
+        self.first_sets = build_first_sets(self)
 
     @property
     def terminals(self):
@@ -139,59 +193,6 @@ class Queue(object):
             return self.pop()
         except IndexError:
             raise StopIteration()
-
-
-def build_first_sets(grammar):
-    """Returns a map from symbols to sets of terminals that can appear as the
-    first terminal in that symbol.  Terminal symbols obviously just map to a
-    set containing only themselves
-
-    This can be created without needing to look at item sets.
-    This requires that the grammar rules are epsilon free.
-    """
-    # A map from symbols to sets of symbols for which there exist productions
-    # where the first symbol is the first element
-    has_first_symbol = {}
-
-    for production in grammar.productions():
-        has_first_symbol.setdefault(
-            production.symbols[0], set()
-        ).add(production.name)
-
-    # A map from symbols to the set of terminals which can appear as the first
-    # terminal in a string that the symbol matches
-    first_sets = {}
-
-    for terminal in grammar.terminals:
-        # TODO if this is needed it should be folded into the inner loop.
-        # kept separate for now so that it's very clear what is happening
-        first_sets[terminal] = {terminal}
-
-        if terminal not in has_first_symbol:
-            continue
-
-        queue = Queue(has_first_symbol[terminal])
-
-        while queue:
-            nonterminal = queue.pop()
-            first_sets.setdefault(nonterminal, set()).add(terminal)
-
-            queue.update(has_first_symbol.get(nonterminal, set()))
-
-    assert (
-        set(first_sets) == set.union(grammar.terminals, grammar.nonterminals)
-    )
-
-    # First sets should contain only terminals
-    assert not any(
-        set.difference(first_set, grammar.terminals)
-        for first_set in first_sets.values()
-    )
-
-    # There should be a first set for every terminal and non-terminal
-    assert grammar.symbols == set(first_sets)
-
-    return first_sets
 
 
 class Item(object):
@@ -316,8 +317,6 @@ def _build_derived_items(grammar, kernel):
 
     # Queue of symbols that should be processed into items
     symbol_queue = Queue()
-
-    grammar.first_sets = build_first_sets(grammar)  # TODO TODO TODO
 
     for item in kernel:
         if not item.expected:
