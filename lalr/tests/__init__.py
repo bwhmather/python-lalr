de3cb71bb475ab3bb275cb95eaa160309ff2eedd
Ben Mather <bwhmather@bwhmather.com>
2016-08-28 14:24:37 +0100

    Ignore mypy failures caused by incorrect type annotations on loadTestsFromModule

--- lalr/tests/__init__.py
+++ lalr/tests/__init__.py
@@ -6,7 +6,7 @@ from lalr.tests import test_parsing
 
 loader = unittest.TestLoader()
 suite = unittest.TestSuite((
-    loader.loadTestsFromModule(test_parsing),
-    loader.loadTestsFromModule(test_analysis),
-    loader.loadTestsFromModule(test_grammar),
+    loader.loadTestsFromModule(test_parsing),  # type: ignore
+    loader.loadTestsFromModule(test_analysis),  # type: ignore
+    loader.loadTestsFromModule(test_grammar),  # type: ignore
 ))
