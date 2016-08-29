0ae22173f6fb5c13e151c97d549c45ad4f17e02c
Ben Mather <bwhmather@bwhmather.com>
2016-08-29 15:22:34 +0100

    Add type annotations to the utils module

--- lalr/utils.py
+++ lalr/utils.py
@@ -1,21 +1,27 @@
-class Queue(object):
-    def __init__(self, items=[]):
-        self._cursor = 0
-        self._by_order = list()
-        self._by_identity = set()
+from typing import TypeVar, Generic, Iterable, Hashable
+
+
+T = TypeVar('T', bound=Hashable)
+
+
+class Queue(Iterable[T], Generic[T]):
+    def __init__(self, items: Iterable[T]=[]) -> None:
+        self._cursor = 0  # type: int
+        self._by_order = list()  # type: List[T]
+        self._by_identity = set()  # type: Set[T]
 
         self.update(items)
 
-    def add(self, item):
+    def add(self, item: T):
         if item not in self._by_identity:
             self._by_order.append(item)
             self._by_identity.add(item)
 
-    def update(self, items):
+    def update(self, items: Iterable[T]):
         for item in items:
             self.add(item)
 
-    def pop(self):
+    def pop(self) -> T:
         if self._cursor == len(self._by_order):
             raise IndexError()
         value = self._by_order[self._cursor]
@@ -23,19 +29,19 @@ class Queue(object):
         return value
 
     @property
-    def processed(self):
+    def processed(self) -> Iterable[T]:
         return self._by_order[:self._cursor]
 
-    def __len__(self):
-        return len(len(self._by_order) - self._cursor)
+    def __len__(self) -> int:
+        return len(self._by_order) - self._cursor
 
-    def __bool__(self):
+    def __bool__(self) -> bool:
         return self._cursor != len(self._by_order)
 
     def __iter__(self):
         return self
 
-    def __next__(self):
+    def __next__(self) -> T:
         try:
             return self.pop()
         except IndexError:
