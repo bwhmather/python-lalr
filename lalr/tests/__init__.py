3be632ab33b8c28ed8f9268362abfbce065c46e8
Ben Mather <bwhmather@bwhmather.com>
2016-08-29 14:47:47 +0100

    Add some tests for the work queue

--- lalr/tests/__init__.py
+++ lalr/tests/__init__.py
@@ -1,4 +1,5 @@
 import unittest
+from lalr.tests import test_queue
 from lalr.tests import test_grammar
 from lalr.tests import test_analysis
 from lalr.tests import test_parsing
@@ -6,6 +7,7 @@ from lalr.tests import test_parsing
 
 loader = unittest.TestLoader()
 suite = unittest.TestSuite((
+    loader.loadTestsFromModule(test_queue),  # type: ignore
     loader.loadTestsFromModule(test_parsing),  # type: ignore
     loader.loadTestsFromModule(test_analysis),  # type: ignore
     loader.loadTestsFromModule(test_grammar),  # type: ignore
---
+++ lalr/tests/test_queue.py
@@ -0,0 +1,56 @@
+import unittest
+from lalr.utils import Queue
+
+
+class ParseTestCase(unittest.TestCase):
+    def test_iteration(self):
+        tasks = ['A', 'B', 'C', 'D', 'E', 'F']
+        queue = Queue(tasks)
+
+        self.assertEqual([task for task in queue], tasks)
+
+    def test_insertion_during_iteration(self):
+        queue = Queue([1])
+
+        for task in queue:
+            if task >= 128:
+                break
+            queue.add(task * 2)
+
+        self.assertEqual(
+            list(queue.processed),
+            [1, 2, 4, 8, 16, 32, 64, 128],
+        )
+
+    def test_reinsertion_before_pop(self):
+        tasks = [1, 2, 3, 4, 5]
+        queue = Queue(tasks)
+
+        processed = []
+        processed += [queue.pop(), queue.pop()]
+        queue.add(3)
+
+        processed += [queue.pop(), queue.pop(), queue.pop()]
+
+        self.assertEqual(processed, tasks)
+        self.assertFalse(queue)
+        self.assertRaises(IndexError, queue.pop)
+
+    def test_reinsertion_after_pop(self):
+        tasks = [1, 2, 3, 4, 5]
+        queue = Queue(tasks)
+
+        processed = []
+        processed += [queue.pop(), queue.pop(), queue.pop()]
+        queue.add(3)
+
+        processed += [queue.pop(), queue.pop()]
+
+        self.assertEqual(processed, tasks)
+        self.assertFalse(queue)
+        self.assertRaises(IndexError, queue.pop)
+
+    def test_processed(self):
+        queue = Queue([1, 2, 3, 4, 5])
+        processed = set([queue.pop(), queue.pop()])
+        self.assertEqual(set(queue.processed), processed)
