94329d9adbe554ca1118b647632a72ebe58bf4fa
Ben Mather <bwhmather@bwhmather.com>
2016-08-29 10:27:19 +0100

    Keep a map from productions to actions instead of passing actions to grammar

--- lalr/__init__.py
+++ lalr/__init__.py
@@ -1,17 +1,28 @@
-from lalr.grammar import Grammar
+from lalr.grammar import Grammar, Production
 from lalr.analysis import ParseTable
 from lalr.parsing import parse
 
 
 class Parser(object):
     def __init__(self, productions, target):
-        grammar = Grammar(productions)
+
+        grammar_productions = []
+        self._actions = {}
+
+        for production in productions:
+            grammar_production = Production(
+                production.name, production.symbols,
+            )
+            self._actions[grammar_production] = production.action
+            grammar_productions.append(grammar_production)
+
+        grammar = Grammar(grammar_productions)
 
         self._parse_table = ParseTable(grammar, target)
 
     def parse(self, tokens, state=None):
         def _action(production, *values):
-            return production.action(*values, state=state)
+            return self._actions[production](*values, state=state)
 
         return parse(self._parse_table, tokens, action=_action)
 
