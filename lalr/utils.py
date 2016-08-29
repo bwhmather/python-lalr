84991252c35cd4ec81865ab18c756a15f3d876e3
Ben Mather <bwhmather@bwhmather.com>
2016-08-29 14:44:16 +0100

    Re-implement queue again using a cursor and list
    This means that the order of processed matches the actual order

--- lalr/utils.py
+++ lalr/utils.py
@@ -1,32 +1,36 @@
-from collections import deque
-
-
 class Queue(object):
     def __init__(self, items=[]):
-        self._queued = deque(items)
-        self._all = set(items)
+        self._cursor = 0
+        self._by_order = list()
+        self._by_identity = set()
+
+        self.update(items)
 
     def add(self, item):
-        if item not in self._all:
-            self._queued.append(item)
-            self._all.add(item)
+        if item not in self._by_identity:
+            self._by_order.append(item)
+            self._by_identity.add(item)
 
     def update(self, items):
         for item in items:
             self.add(item)
 
     def pop(self):
-        return self._queued.popleft()
+        if self._cursor == len(self._by_order):
+            raise IndexError()
+        value = self._by_order[self._cursor]
+        self._cursor += 1
+        return value
 
     @property
     def processed(self):
-        return iter(set.difference(self._all, self._queued))
+        return self._by_order[:self._cursor]
 
     def __len__(self):
-        return len(self._queued)
+        return len(len(self._by_order) - self._cursor)
 
     def __bool__(self):
-        return bool(self._queued)
+        return self._cursor != len(self._by_order)
 
     def __iter__(self):
         return self
