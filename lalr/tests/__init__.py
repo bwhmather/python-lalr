3fab06544fe4a49fd1072ae9037de06cbed48fce
Ben Mather <bwhmather@bwhmather.com>
2020-11-07 09:12:57 +0000

    Run example tests

--- lalr/tests/__init__.py
+++ lalr/tests/__init__.py
@@ -1,6 +1,8 @@
 import unittest
 
-from lalr.tests import test_analysis, test_grammar, test_parsing, test_queue
+from lalr.tests import (
+    test_analysis, test_examples, test_grammar, test_parsing, test_queue,
+)
 
 
 loader = unittest.TestLoader()
@@ -9,4 +11,5 @@ suite = unittest.TestSuite((
     loader.loadTestsFromModule(test_parsing),
     loader.loadTestsFromModule(test_analysis),
     loader.loadTestsFromModule(test_grammar),
+    loader.loadTestsFromModule(test_examples)
 ))
---
+++ lalr/tests/test_examples.py
@@ -0,0 +1,40 @@
+import unittest
+
+from lalr import Grammar, ParseTable, Production, parse
+from lalr.exceptions import ParseError
+
+
+def nop(production, *args):
+    return production.name
+
+
+class LispTestCase(unittest.TestCase):
+    grammar = Grammar([
+        Production("list", ("lparen", "rparen")),
+        Production("list", ("lparen", "list_body", "rparen")),
+
+        Production("list_body", ("expression",)),
+        Production("list_body", ("expression", "list_body")),
+
+        Production("expression", ("list",),),
+        Production("expression", ("string",)),
+        Production("expression", ("number",)),
+        Production("expression", ("symbol",)),
+    ])
+
+    parse_table = ParseTable(grammar, "expression")
+
+    def test_single_element_list(self):
+        parse(self.parse_table, ["lparen", "string", "rparen"], action=nop)
+
+    def test_missing_closing_paren(self):
+        with self.assertRaises(ParseError) as exc_context:
+            parse(self.parse_table, ["lparen", "string"], action=nop)
+
+        exc = exc_context.exception
+        self.assertEqual(exc.lookahead_token, None)
+
+        self.assertEqual(
+            exc.valid_shifts, {"lparen", "symbol", "number", "string"}
+        )
+        self.assertEqual(exc.valid_reductions, {"rparen"})
