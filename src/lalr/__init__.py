4b55887246c903f7a9e990eb4fed7df0c57cb0df
Ben Mather <bwhmather@bwhmather.com>
2021-10-28 20:40:48 +0100

    Read version from lalr.__version__

--- src/lalr/__init__.py
+++ src/lalr/__init__.py
@@ -3,6 +3,8 @@ from lalr.exceptions import ProductionSpecParseError
 from lalr.grammar import Grammar, Production
 from lalr.parsing import parse
 
+__version__ = "0.0.1"
+
 __all__ = [
     "ParseTable",
     "ProductionSpecParseError",
