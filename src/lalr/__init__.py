8a98db00c05ee7c13a5e5ec7b580d28e04cb43ca
Ben Mather <bwhmather@bwhmather.com>
2022-01-30 00:11:52 +0000

    Release 0.2.0

--- src/lalr/__init__.py
+++ src/lalr/__init__.py
@@ -3,7 +3,7 @@ from lalr.exceptions import ProductionSpecParseError
 from lalr.grammar import Grammar, Left, Precedence, Production, Right
 from lalr.parsing import parse
 
-__version__ = "0.1.2"
+__version__ = "0.2.0"
 
 __all__ = [
     "ParseTable",
